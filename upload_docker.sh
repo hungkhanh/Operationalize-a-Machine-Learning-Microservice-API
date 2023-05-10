8fd2f8d8116bf33eb3c5c90442d04de2956638cc
hung pham <hungpk1412@gmail.com>
2023-05-10 08:55:42 +0000

    upload the docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=honghot/housing-price:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
